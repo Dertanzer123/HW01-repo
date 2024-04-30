2874ab4e7a9df2db48d38bef9b3460985f9912e4
Emin Tekin <emint2719gmail.com>
2024-04-30 18:32:32 +0300

    update to game and linkedlist
    new methods were added for needed functionalities of this project

--- Hw01/src/Hw01/game.java
+++ Hw01/src/Hw01/game.java
@@ -1,25 +1,73 @@
 package Hw01;
 
+import java.util.Random;
+
 public class game {
 
 	linkedlist playerlist;
 	linkedlist computerlist;
 	linkedlist tablelist;
+	Random rnd=new Random();
+	
+	int turnnumber;
 	
 	game()
 	{
 		playerlist=new linkedlist();
 		computerlist=new linkedlist();
 		tablelist =new linkedlist();
+		int card;
+		int[] cards=new int[6];
 		
 		
+		for(int i=0;i<24;i+=0)
+		{
+			while(true) {
+			card=rnd.nextInt(6)+1;
+			if(cards[card]<4) 
+			{
+			cards[card]++;
+			tablelist.add(card);
+			}
+			}
+			
+		}
+		for(int i=0;i<14;i++)
+		{
+			drawcard(i%2==0);// i think the thing i made here is sooooo cool rn 
+			
+		}
 		
 		
 		
 	}
+	public void drawcard(boolean secgec)//if secgec=true then player else if false then computer draws an card
+	{
+		
+		
+		int int1=rnd.nextInt(tablelist.size());
+		
+		if(secgec) 
+		{
+			playerlist.add(tablelist.getelement(int1));
+		}
+		else 
+		{
+			computerlist.add(tablelist.getelement(int1));
+		}
+		tablelist.discardelement(int1);
+	}
+	public void display()
 	
+	{
+		System.out.println(turnnumber+"                          Table");
+		System.out.print("YoU      : ");
+		playerlist.display();
+		System.out.print("Computer : ");
+		computerlist.display();
+		System.out.println("---------------------------------");
 	
-	
+	}
 	
 	
 	
--- Hw01/src/Hw01/linkedlist.java
+++ Hw01/src/Hw01/linkedlist.java
@@ -3,30 +3,93 @@ package Hw01;
 public class linkedlist {
 
 	private node head;
-	
-	linkedlist()
-	{
-		head =null;
+
+	linkedlist() {
+		head = null;
 	}
-	
-	public void add(Object data)
-	{
-		if(head==null) 
-		{	
-			
-			head=new node(data);
-		}
-		else 
-		{
-			node temp1=head;
-			
-			while(temp1.getLink()!=null) 
-			{
-				temp1=temp1.getLink();
+
+	public void add(Object data) {
+		if (head == null) {
+
+			head = new node(data);
+		} else {
+			node temp1 = head;
+
+			while (temp1.getLink() != null) {
+				temp1 = temp1.getLink();
 			}
-			node newnode= new node(data);
+			node newnode = new node(data);
 			temp1.setLink(newnode);
 		}
+	}
+
+	public boolean discardelement(int index) {
+
+		if (head == null) {
+			return false;
+		}
+		if (index == 0) {
+			head = head.getLink();
+			return true;
+		}
+
+		node temp = head;
+		for (int i = 1; i < index; i++) {
+			if (temp == null) {
+				return false;
+			}
+			temp = temp.getLink();
+		}
+		if (temp == null || temp.getLink() == null) {
+			return false;
+		}
+		if (temp.getLink().getLink() != null) {
+			temp.setLink(temp.getLink().getLink());
+		} else {
+			temp.setLink(null);
+		}
+		return true;
+	}
+	public Object getelement(int index)
+	{
+		node temp=head;
+		for (int i = 0; i < index; i++) {
+			if (temp == null) {
+				System.out.println("index is out of bounds");
+				return false;
+			}
+			temp = temp.getLink();
+		}
+		if (temp == null) {
+			System.out.println("index is out of bounds");
+			return false;
+		}
+		return temp.getData();
+	}
+	public void display()
+	
+	{
+		node temp=head;
+		while(temp!=null)
+		{
+			System.out.print(temp.getData()+" ");
+			temp=temp.getLink();
+		}
+		
+	}
+	// public boolean discardelement(Object data)
+	// {
+	//
+	// }
+
+	public int size() {
+		int size = 0;
+		node temp = head;
+		while (temp != null) {
+			temp = temp.getLink();
+			size++;
+		}
+		return size;
 	}
 
 	public node getHead() {
@@ -36,5 +99,5 @@ public class linkedlist {
 	public void setHead(node head) {
 		this.head = head;
 	}
-	
+
 }
